cc134023c8ab8598b84686968fd6a0f42616e4e0
shubham <shubham@getsafle.com>
2022-01-18 14:07:34 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const ethereum = require('./contract-map/ethereum.json');
+const polygon = require('./contract-map/polygon.json');
+const supportedChains = require('./supportedChains.json');
+module.exports={
+    ethereum, polygon, supportedChains,
+}
